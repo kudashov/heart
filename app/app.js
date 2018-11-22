b482f887643d71a74c61ae51f3b0ea23e356b1bb
Kudashov Ivan <i.dev7788@gmail.com>
2018-11-22 22:29:10 +0300

    fix

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
-app.get('/', function(req, res){
-   res.send("Hello World!");
-});
-app.listen(8881, 'localhost');
+const http = require('http');
+
+const hostname = '0.0.0.0'; // listen on all ports
+const port = 1337;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+    }).listen(port, hostname, () => {
+      console.log('Server running at http://${hostname}:${port}/');
+      });
