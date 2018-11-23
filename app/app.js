d347910e070cd53c0e2930d62b782654772b1fa7
Kudashov Ivan <i.dev7788@gmail.com>
2018-11-23 06:59:15 +0300

    fix

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '0.0.0.0'; // listen on all ports
-const port = 1337;
+const hostname = '127.0.0.1'; // listen on all ports
+const port = 8881;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
