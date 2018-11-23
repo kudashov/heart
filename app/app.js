a001fc21c7ec3c0b2b29fa6914190cc26d962d61
Ivan <i.devel@rambler.ru>
2018-11-23 09:04:39 +0500

    format

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ const port = 8881;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-    }).listen(port, hostname, () => {
-      console.log('Server running at http://${hostname}:${port}/');
-      });
+  res.end('Hello World\n');
+}).listen(port, hostname, () => {
+  console.log('Server running at http://${hostname}:${port}/');
+});
+
